b0d477299f8448889ac0f9e80b4948e6b2ed84d0
lieby weintraub <liebahinda@gmail.com>
2019-04-30 18:33:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ class Student
     DB[:conn].execute(sql)
   end 
   def update 
-    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
 
     
