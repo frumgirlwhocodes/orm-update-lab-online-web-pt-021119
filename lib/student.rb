85cc989858dd4dc38012121a65f1730ae93e1651
lieby weintraub <liebahinda@gmail.com>
2019-04-30 18:36:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,8 +29,8 @@ class Student
   end 
   def update 
     sql = <<-SQL
-    UPDATE students SET name = ?, grade = ? WHERE id = ?
-    SQL 
+    UPDATE students SET name = ?, grade = ? WHERE id = ? 
+    SQL
     
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end 
